477b3433e2f2696417eb707113b327bdbccf313f
luis enrique olvera calderon <leoc_2911@hotmail.com>
2021-03-23 23:02:14 -0600

    Employee service implementation

---
+++ VogCodeChallenge.Domain/Services/EmployeeManagementService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using VogCodeChallenge.Domain.EmployeeManagement;
+
+namespace VogCodeChallenge.Domain.Services
+{
+    public class EmployeeManagementService : IEmployeeManagementService
+    {
+        public IEnumerable<Employee> GetAll()
+        {
+            return GetTestData();
+        }
+
+        public IList<Employee> ListAll()
+        {
+            return GetTestData().ToList();
+        }
+
+        private IEnumerable<Employee> GetTestData()
+        {
+            yield return new Employee("Luis", "Olvera", "Software Engineer", new Address() { FullAddress = "Mexico City" });
+            yield return new Employee("Elon", "Musk", "Founder", new Address() { FullAddress = "California" });
+            yield return new Employee("Jeff", "Bezos", "CEO", new Address() { FullAddress = "New York" });
+            yield return new Employee("Satya", "Nadella", "CIO", new Address() { FullAddress = "Washington" });
+        }
+    }
+}
---
+++ VogCodeChallenge.Domain/Services/IEmployeeManagementService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using VogCodeChallenge.Domain.EmployeeManagement;
+
+namespace VogCodeChallenge.Domain.Services
+{
+    public interface IEmployeeManagementService
+    {
+        IEnumerable<Employee> GetAll();
+        IList<Employee> ListAll();
+    }
+}
