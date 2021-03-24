1ea586c98d8180d5a757915e3f01cfa4ca973856
luis enrique olvera calderon <leoc_2911@hotmail.com>
2021-03-23 22:02:36 -0600

    Initial structure for API project

---
+++ VogCodeChallenge.API/Controllers/EmployeesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        [HttpGet]        
+        public List<string> GetAll()
+        {
+            return new List<string>() { "Hello", "World" };
+        }
+    }
+}
