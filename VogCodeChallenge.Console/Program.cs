f07c818c7c30ab71e1d3fa30d86085adafd2dfcd
luis enrique olvera calderon <leoc_2911@hotmail.com>
2021-03-25 00:47:43 -0600

    Added Iterations Console project

---
+++ VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VogCodeChallenge.Console.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+
+            QuestionClass.IterationMethod1();
+            System.Console.WriteLine("------------------");
+            QuestionClass.IterationMethod2();
+        }
+    }
+
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John",
+        };
+
+        public static void IterationMethod1()
+        {
+            System.Console.WriteLine("Iteration method 1 -> using while");
+
+            var listLength = NamesList.Count;
+            var indexer = 0;
+
+            while (indexer < listLength) 
+            {
+                System.Console.WriteLine(NamesList[indexer]);
+                indexer++;
+            }            
+        }
+
+        public static void IterationMethod2()
+        {
+            System.Console.WriteLine("Iteration method 2 -> using Linq Select");
+
+            NamesList.Select(name => { System.Console.WriteLine(name); return name; }).ToList();
+        }
+    }
+
+}
